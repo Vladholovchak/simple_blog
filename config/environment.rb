c1de1daa5bf948afc0ab898fc82a94c8fdd23b2f
Vlad <vlad.golovchak.1996@gmail.com>
2021-12-01 16:53:10 +0200

    remove not using files and configs, add user entity

--- config/environment.rb
+++ config/environment.rb
@@ -11,13 +11,6 @@ Hanami.configure do
     ##
     # Database adapter
     #
-    # Available options:
-    #
-    #  * SQL adapter
-    #    adapter :sql, 'sqlite://db/simple_blog_development.sqlite3'
-    #    adapter :sql, 'postgresql://localhost/simple_blog_development'
-    #    adapter :sql, 'mysql://localhost/simple_blog_development'
-    #
     adapter :sql, ENV.fetch('DATABASE_URL')
 
     ##
@@ -27,23 +20,11 @@ Hanami.configure do
     schema     'db/schema.sql'
   end
 
-  mailer do
-    root 'lib/simple_blog/mailers'
-
-    # See https://guides.hanamirb.org/mailers/delivery
-    delivery :test
-  end
-
   environment :development do
-    # See: https://guides.hanamirb.org/projects/logging
     logger level: :debug
   end
 
   environment :production do
     logger level: :info, formatter: :json, filter: []
-
-    mailer do
-      delivery :smtp, address: ENV.fetch('SMTP_HOST'), port: ENV.fetch('SMTP_PORT')
-    end
   end
 end
